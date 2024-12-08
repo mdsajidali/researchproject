fc0d5207c4d9217d22bc13a70ed7c962869732ca
Sajid <ec2-user@ip-172-31-29-145.eu-west-1.compute.internal>
2024-12-08 02:24:10 +0000

    changes35

--- expense_tracker/expense_tracker/settings.py
+++ expense_tracker/expense_tracker/settings.py
@@ -119,11 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+"""STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'expenses', 'static'),
-]
+]"""
 # Add or update the STATIC_ROOT setting
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
